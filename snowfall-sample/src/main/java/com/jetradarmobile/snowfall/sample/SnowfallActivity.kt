1ca9ea2da3c78c96ae03d80996fcf9e8cad00ca8
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-04-08 13:19:37 +0200

    Update sample activity

--- snowfall-sample/src/main/java/com/jetradarmobile/snowfall/sample/SnowfallActivity.kt
+++ snowfall-sample/src/main/java/com/jetradarmobile/snowfall/sample/SnowfallActivity.kt
@@ -1,12 +1,21 @@
 package com.jetradarmobile.snowfall.sample
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jetradarmobile.snowfall.SnowfallView
 
 class SnowfallActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_snowfall)
+
+    findViewById<SnowfallView>(R.id.snowfall).setSnowflakeBitmaps(listOf(
+      BitmapFactory.decodeResource(resources, R.drawable.egg),
+      BitmapFactory.decodeResource(resources, R.drawable.egg2),
+      BitmapFactory.decodeResource(resources, R.drawable.egg3),
+      BitmapFactory.decodeResource(resources, R.drawable.egg4)
+    ))
   }
 }
